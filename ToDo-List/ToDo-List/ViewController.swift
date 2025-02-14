8503b78e45df4090a1ea7b316331144240cc84b9
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-02-14 18:05:51 -0400

    Config initial

---
+++ ToDo-List/ToDo-List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo-List
+//
+//  Created by Maxwell Silva on 14/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
